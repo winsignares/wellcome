eb3ac0a9b6499edeb87835373793a190705bd105
JohnYL47 <jylaguna6@misena.edu.co>
2022-10-16 15:01:52 -0500

    Registro JS mejorado

--- Jonatan/dinamicajs.js
+++ Jonatan/dinamicajs.js
@@ -1,36 +1,56 @@
-/*console.log("Jajaja a empezado el registro!");
-var Login_Or_Regist = prompt("Bienvenido a JS!\nTiene cuenta en esta app?");
 var usuariocreado;
-var num = 1;
-var confirmaciones_si = ["si", "yes","Si", "SI","YES"];
-var confirmaciones_no = ["no", "nou", "No", "nO", "NO"];
+var num = 2;
 
 for (let i = 0; i < num; i++) {
-    
-    if (Login_Or_Regist == confirmaciones_si) {
 
-        alert(usuariocreado);
-} else if (Login_Or_Regist == "no") {
-    
-    var nombre_usuario_reg = prompt("Ingrese un nombre de usuario para el registro");
-    var passwd = prompt("ingrese una contraseña");
-    var confirmacion = prompt("Quiere tener "+nombre_usuario_reg+" como nombre de usuario?");
-    
-    if (confirmacion == confirmaciones_si) {
+    var Login_Or_Regist = prompt("Bienvenido a JS!\nTiene cuenta en esta app?  (si/no)");
 
-        var conf_usuario = prompt("Usuario registrado!\nIngrese su nombre de usuario nuevamente");
-        var conf_passwd = prompt("Ingrese su contraseña de usuario");
+    if (Login_Or_Regist == "si") {
 
-        if  (conf_usuario == nombre_usuario_reg || conf_passwd == passwd) {
-            
-            usuariocreado = conf_usuario;
-            //return usuariocreado;
-    } else {
-        alert("Favor ingrese bien la contraseña")
+        var validar_nombre = prompt("Ingrese su nombre de usuario para validar");
+        var validar_passwd = prompt("Ingrese su contraseña");
+
+        if (contraseña_creado == validar_passwd || usuariocreado == validar_nombre) {
+            alert("Bienvenido "+usuariocreado+"!");
+        } else{
+            alert("Favor ingrese bien la contraseña");
+            num++;
+        }
+
+    } else if (Login_Or_Regist == "no") {
+        var h = 1;
+        for (let j = 0; j < h; j++) {
+
+            var nombre_usuario_reg = prompt("Ingrese un nombre de usuario para el registro");
+            var passwd = prompt("ingrese una contraseña");
+            var confirmacion = prompt("Quiere tener '" + nombre_usuario_reg + "' como nombre de usuario?");
+
+            if (confirmacion == "si") {
+
+                var conf_usuario = prompt("Usuario registrado!\nIngrese su nombre de usuario nuevamente");
+                var conf_passwd = prompt("Ingrese su contraseña de usuario");
+
+                if (conf_usuario == nombre_usuario_reg || conf_passwd == passwd) {
+                                        
+                    usuariocreado = conf_usuario;
+                    contraseña_creado = conf_passwd;
+                    alert("Se realizo bien "+ usuariocreado);
+                    break;                    
+
+                } else {
+                    alert("Favor ingrese bien la contraseña o usuario")
+                    h++;
+                }
+
+            } else if (confirmacion == "no") {
+                h++;
+            } else {
+                alert("Favor oprimir ('si' o 'no')");
+                h++;
+            }
+
+        }
+    } else{
+        alert("Solamente ingrese (si/no)");
     }
-
-} else {
-    alert("Favor oprimir ('si' o 'no')");
 }
-}
-}*/
